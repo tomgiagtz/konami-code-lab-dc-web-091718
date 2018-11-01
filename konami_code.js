81bc709c14fec3152c63b814cdd8c79506b97fa0
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-10-31 20:54:44 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,33 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
 ];
 
 function init() {
-  // your code here
+    index = 0;
+
+    body = document.querySelector("body");
+    body.addEventListener("keydown", konamiCheck);
+
+    function konamiCheck(e) {
+        
+        let pressedKey = event.key;
+        if (pressedKey === codes[index]) {
+            index++;
+        } else {
+            index = 0;
+        }
+
+        if (index === codes.length) {
+            alert("woah");
+        }
+    }
 }
